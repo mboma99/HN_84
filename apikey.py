325260fbacc73bbbad35b460bc93357259eeb61f
James Mboma <42808235+mboma99@users.noreply.github.com>
2023-11-05 09:49:31 +0000

    Update apikey.py

--- apikey.py
+++ apikey.py
@@ -1,3 +1,3 @@
-apikey = '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'
+apikey = 'OPEN API KEY HERE'
 
-serp_apikey = 'bae683d4f39ee8eecc8e03c56c1ebdba6a6613ffcb6a76a770e6c83ab07faec6'
+serp_apikey = 'SERP API KEY HERE'
