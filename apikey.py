37349bd2f41613426b9f7ddd69794fbbec1bd7f3
james <jamesmboma99@gmail.com>
2023-12-05 15:24:45 +0000

    refactored

--- apikey.py
+++ apikey.py
@@ -1,4 +1,4 @@
 
-apikey = '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'
+apikey = 'Open API key here '
 
-serp_apikey = 'bae683d4f39ee8eecc8e03c56c1ebdba6a6613ffcb6a76a770e6c83ab07faec6'
+serp_apikey = 'SERP API Key here'
